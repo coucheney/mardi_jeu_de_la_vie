b55184580092ad5052e4765fc7fcb87df8c5e6f7
coucheney <pierre.coucheney@uvsq.fr>
2021-02-02 17:11:28 +0100

    ajout du programme

---
+++ jeu_de_la_vie.py
@@ -0,0 +1,8 @@
+####################################################
+# Auteurs: 
+# Pierre Coucheney
+# Toto
+# Groupe:
+# BI 5
+# https://github.com/coucheney/mardi_jeu_de_la_vie
+###################################################
