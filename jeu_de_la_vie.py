fbcb047e6ce519db37a1bcccf7f41aef218ce582
coucheney <pierre.coucheney@uvsq.fr>
2021-02-02 17:19:22 +0100

    début du programme

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -6,3 +6,23 @@
 # BI 5
 # https://github.com/coucheney/mardi_jeu_de_la_vie
 ###################################################
+
+#####################
+# import des modules
+
+import tkinter as tk
+
+
+###################
+# fonctions
+
+
+
+#####################
+# programme principal
+
+racine = tk.Tk()
+racine.title("Jeu de la vie")
+racine.mainloop()
+
+
